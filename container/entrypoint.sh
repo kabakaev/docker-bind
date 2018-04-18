a024af1ccd8d69e28dfea7229a0221718d0b5574
Alexander Kabakaev <kabakaev@gmail.com>
2018-04-18 12:48:42 +0000

    generate rndc key if it does not exist

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -5,5 +5,9 @@ set -x
 #chown -R root:named /etc/bind /var/cache/bind /var/run/named
 #chmod -R 770 /var/cache/bind /var/run/named
 #chmod -R 750 /etc/bind
+if [ ! -f /etc/bind/rndc.key ] ; then
+    rndc-confgen -a 
+    chown named /etc/bind/rndc.key
+fi
 # Run in foreground and log to STDERR (console):
 exec /usr/sbin/named -c /etc/bind/named.conf -g -u named
