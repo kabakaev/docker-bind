a9eb2cf6b7a6f253acf27b47693aa9995460940c
Alexander Kabakaev <kabakaev@gmail.com>
2018-12-14 12:50:14 +0000

    enable host netwrok; merge from upstream

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -10,4 +10,4 @@ if [ ! -f /etc/bind/rndc.key ] ; then
     chown named /etc/bind/rndc.key
 fi
 # Run in foreground and log to STDERR (console):
-exec /usr/sbin/named -c /etc/bind/named.conf -g -u named
+exec /usr/sbin/named -c /etc/bind/named.conf -g -u named $OPTIONS
--- start.sh
+++ start.sh
@@ -2,10 +2,11 @@ sudo mkdir -p /data/bind/etc/bind /data/bind/var/cache/bind
 
 sudo docker rm -f dns-master
 
+#  -p 53:53/udp -p 53:53 \
+#  -p 127.0.0.1:953:953 \
 sudo docker run --name=dns-master -d \
+  --net=host \
   --dns=1.1.1.1 --dns=8.8.4.4 \
-  -p 53:53/udp -p 53:53 \
-  -p 127.0.0.1:953:953 \
   -v /data/bind/etc/bind:/etc/bind \
   -v /data/bind/var/cache/bind:/var/cache/bind \
 kabakaev/bind
